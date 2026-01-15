23d31b35bf757bf30efa08f74d3731a2d5a1578f
yoohengu <yoohgxxx@gmail.com>
2026-01-15 22:35:26 +0900

    20260115

---
+++ PYTHON/PROGRAMMERS/코딩테스트 입문/두 수의 차 구하기.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120803?language=python3
+def solution(num1, num2):
+    if -50000<=num1<=50000 and -50000<=num2<= 50000:
+        answer = num1 - num2
+        return answer
---
+++ PYTHON/PROGRAMMERS/코딩테스트 입문/몫 구하기.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120805
+def solution(num1, num2):
+    if 0<=num1<=100 and 0<=num2<= 100:
+        answer = int(num1 /num2)
+        return answer
