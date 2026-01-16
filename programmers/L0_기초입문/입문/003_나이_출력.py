b00c07b960f5fee0bf6f2981da72c097b0091165
yoohengu <yoohgxxx@gmail.com>
2026-01-16 22:45:54 +0900

    [프로그래머스] L0 - 나이 출력 (#3)

---
+++ programmers/L0_기초입문/입문/003_나이_출력.py
@@ -0,0 +1,10 @@
+# 나이 출력
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 01. 16. 22:45:53
+
+def solution(age):
+    answer = 2022 - age + 1
+    return answer
